78664e9e517aa1c50d659a90c3dd70157c5af685
Rumman <rumman.khan@tothenew.com>
2016-01-18 15:09:42 +0530

    venue and contact updated

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -47,14 +47,41 @@
           jQuery(this).removeClass('speaker-animation');
         });*/
 
-        jQuery(window).scroll(function() {
-          if (jQuery(this).scrollTop() > 490){
-            jQuery('.header-top').addClass('smaller');
+          if (jQuery(window).width() > 760) {
+            jQuery(window).scroll(function () {
+              if (jQuery(this).scrollTop() > 154) {
+                jQuery('.header-top').addClass('smaller');
+              }
+
+              else {
+                jQuery('.header-top').removeClass('smaller');
+              }
+            });
           }
-          else{
-            jQuery('.header-top').removeClass('smaller');
+
+
+        jQuery(".header-top .logo img").click(function(){
+          var target = $(".banner-area");
+          jQuery('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+
+          });
+        jQuery(window).scroll(function () {
+          if (jQuery(this).scrollTop() > 400) {
+            jQuery('.scroll-top').show();
+          }
+          else  {
+            jQuery('.scroll-top').hide();
           }
         });
+
+        jQuery('.scroll-top').on("click",function(){
+          var target = $(".banner-area");
+          jQuery('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+        } )
 
         jQuery(function(){
           jQuery.scrollIt({
