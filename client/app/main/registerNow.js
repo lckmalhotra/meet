327cc8a23100188c15a62a2b84199ae42d56561c
Rumman <rumman.khan@tothenew.com>
2016-01-22 14:32:30 +0530

    form functionality updated

--- client/app/main/registerNow.js
+++ client/app/main/registerNow.js
@@ -11,12 +11,16 @@
             $scope.formData = {};
             $scope.isActive = true;
 
+
             $scope.addThing = function () {
+              if($scope.userForm.$valid) {
+                console.log("run");
                 if ($scope.formData) {
-                    $http.post('/api/registrations', $scope.formData);
-                    $scope.formData = {};
-                    $scope.isActive = false;
+                  $http.post('/api/registrations', $scope.formData);
+                  $scope.formData = {};
+                  $scope.isActive = false;
                 }
+              }
             };
             $scope.closeThisDialog = function () {
                 ngDialog.close();
