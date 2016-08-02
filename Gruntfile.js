aec6478b7b1e849ddfebb021a9ad3dd5c3314efe
lckmalhotra <lck.malhotra@gmail.com>
2016-08-03 00:26:23 +0530

    Grunt image/font path issue resolved.

--- Gruntfile.js
+++ Gruntfile.js
@@ -375,8 +375,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             'bower_components/**/*',
-            'assets/images/{,*/}*.{webp}',
-            'assets/font/**/*',
+            'assets/**/*',
             'index.html'
           ]
         }, {
